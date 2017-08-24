27dc8729f8b3b737a1b007f72b135c5efeeb7abf
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-24 10:37:33 +0200

    Wire in redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import tasksApp from './reducers';
+
+let store = createStore(tasksApp, applyMiddleware(thunk));
+console.log(store);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
