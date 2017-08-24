f99f4c2d9bd92a4c372a95fe85195368249a21c3
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-24 10:10:28 +0200

    Extract separate Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
-import logo from './logo.svg';
+import {Home} from './components/HomePage';
 import './App.css';
-
-const Home = () => {
-    return (
-        <div>
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to TaskManager</h2>
-            </div>
-        </div>
-    );
-};
 
 class App extends Component {
   render() {
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+export const Home = () => {
+    return (
+        <div>
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to TaskManager</h2>
+            </div>
+        </div>
+    );
+};
