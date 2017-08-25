7c40743afb30f86bb7c3880468ce7b7ef239f5cd
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-25 15:12:07 +0200

    Proper login form added

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -15,18 +15,39 @@ const mapStateToProps = (state) => {
     return {};
 }
 
-const LoginPage = (props) => {
-    return (
-        <div className="App-intro">
-        <p>Please log in to use the service</p>
-        <button onClick={e => {
-            e.preventDefault();
-            props.onLoginClick('user1@localhost', 'password1')
-        }}>
-            Login
-        </button>
-        </div>
-    );
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {username: '', password: ''};
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+    }
+
+    onUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    onPasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="App-intro">
+                <p>Please log in to use the service</p>
+                <form className="login">
+                    <input type="text" name="username" placeholder="Username"
+                        onChange={this.onUsernameChange} />
+                    <input type="password" name="password" placeholder="Password"
+                        onChange={this.onPasswordChange} />
+                    <input type="button" value="Log in" onClick={e => {
+                        e.preventDefault();
+                        this.props.onLoginClick()
+                    }}/>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
