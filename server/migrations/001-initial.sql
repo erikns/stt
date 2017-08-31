3d5059ac1fb6a474fa37be15b490bea53627966d
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-31 09:45:22 +0200

    Better names of test tasks

--- server/migrations/001-initial.sql
+++ server/migrations/001-initial.sql
@@ -1,8 +1,9 @@
 -- Up
 CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,
     done INTEGER NOT NULL);
-INSERT INTO task (name, done) VALUES ('Test task SQL 1', 0);
-INSERT INTO task (name, done) VALUES ('Test task SQL 2', 0);
+INSERT INTO task (name, done) VALUES ('Easy task', 1);
+INSERT INTO task (name, done) VALUES ('Medium task', 0);
+INSERT INTO task (name, done) VALUES ('Hard task', 0);
 
 CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL);
