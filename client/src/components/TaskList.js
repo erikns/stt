8676a0d8311e9e86dd5d70913da5e6ff58d5c4f1
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-31 10:18:02 +0200

    Refactor rendering of tasks

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const TaskList = (props) => {
+    const doneButton = (done) => {
+        if (done) {
+            return <div className="right">mark undone</div>;
+        } else {
+            return <div className="right">mark done</div>;
+        }
+    };
+
     const taskItems = props.tasks.map((task) => {
         if (task.done === 0) {
-            return <li key={task.id}>{task.name}</li>
+            return <li key={task.id}>{task.name}{doneButton(false)}</li>
         } else {
-            return <li key={task.id} className="done">{task.name}</li>
+            return <li key={task.id} className="done">{task.name}{doneButton(true)}</li>
         }
     });
+
     return (
         <div>
             <h1>Tasks</h1>
