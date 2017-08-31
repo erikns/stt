4ac761c7e70dab08c0982ad85f2b0025a1ca3d70
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-31 09:42:55 +0200

    Design task list. Mark border green when done

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const TaskList = (props) => {
     const taskItems = props.tasks.map((task) => {
-        return <li key={task.id}>{task.name}</li>
+        if (task.done === 0) {
+            return <li key={task.id}>{task.name}</li>
+        } else {
+            return <li key={task.id} className="done">{task.name}</li>
+        }
     });
     return (
         <div>
             <h1>Tasks</h1>
-            <ul>
+            <ul className="tasks">
                 {taskItems}
             </ul>
         </div>
